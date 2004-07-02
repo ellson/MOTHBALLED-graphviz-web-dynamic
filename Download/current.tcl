768ec10d2d817d5df20f692d6194884b129097d5
ellson <devnull@localhost>
2004-07-02 20:22:23 +0000

    Initial revision

---
+++ Download/current.tcl
@@ -0,0 +1,74 @@
+#!/usr/bin/tclsh
+                                                                                
+set docroot /var/www/html
+set archive pub/graphviz/ARCHIVE
+set current pub/graphviz/CURRENT
+                                                                                
+proc puts_latest {fout docroot dir} {
+  set types {tar.gz src.rpm i386.rpm exe}
+  set regexp {([-a-z]*)([-0-9.]*)([a-z][.a-z0-9]*)}
+
+  if {![file exists $docroot/$dir]} {
+    puts $fout "<tr><td align=left><font color=\"red\">Directory \"$docroot/$dir/\" was not found.</font></td></tr>"
+    return
+  }
+
+  set owd [pwd]
+  cd $docroot/$dir
+  foreach {fn n v t} [regexp -all -inline $regexp [glob -nocomplain *]] {
+    if {![file isdir $fn] && [string first graphviz $fn] == 0} {
+      lappend NAMETYPES([list $n $t]) [list $fn $v]
+    }
+  }
+
+  foreach nt [array names NAMETYPES] {
+    foreach {n t} $nt {break}
+    set fnv [lindex [lsort -decreasing -dictionary -index 1 $NAMETYPES($nt)] 0]
+    foreach {fn v} $fnv {break}
+    lappend FILES($t) $fn
+  }
+  cd $owd
+  foreach t $types {
+    if {[info exists FILES($t)]} {
+      puts $fout "<tr><td align=left>"
+      foreach fn [lsort $FILES($t)] {
+        puts $fout "<a href=\"/$dir/$fn\">$fn</a><br>"
+      }
+      puts $fout "</td></tr>"
+    }
+  }
+}
+
+set fout [open Download.ht w]
+
+puts $fout "<table><tr><td>"
+                                                                                
+# the latest release is in ARCHIVE
+puts $fout "<table rules=\"all\" width=\"100%\">"
+puts $fout "<tr><th align=\"left\">Release</th></tr>"
+puts_latest $fout $docroot $archive
+puts $fout "</table>"
+
+puts $fout "</td></tr><tr><td>&nbsp;</td></tr><tr><td>"
+
+# the latest snapshot is in CURRENT
+puts $fout "<table rules=\"all\" width=\"100%\">"
+puts $fout "<tr><th align=\"left\">Development Snapshot</th></tr>"
+puts_latest $fout $docroot $current
+puts $fout "</table>"
+
+puts $fout "</td></tr><tr><td>&nbsp;</td></tr><tr><td>"
+
+# CVS instructions
+puts $fout "<table rules=\"all\" width=\"100%\">"
+puts $fout "<tr><th align=\"left\">CVS</th></tr>"
+puts $fout "<tr><td align=left>"
+puts $fout "<b>cvs -d :pserver:anoncvs@cvs-graphviz.research.att.com:/home/cvsroot login</b><br>"
+puts $fout "<i>(password is \"anoncvs\")</i></br>"
+puts $fout "<b>cvs -d :pserver:anoncvs@cvs-graphviz.research.att.com:/home/cvsroot co graphviz</b><br>"
+puts $fout "</td></tr>"
+puts $fout "</table>"
+
+puts $fout "</td></tr></table>"
+
+close $fout
---
+++ Makefile
@@ -0,0 +1,28 @@
+PAGESET=Home.html \
+	News.html \
+	Screenshots.html \
+	Documentation.html \
+	Bugs.html \
+	MailingList.html \
+	License.html \
+	Download.html \
+	Links.html \
+	Credits.html
+
+.SUFFIXES: .ht .html
+
+.ht.html:
+	./ht2html.py "${PAGESET}" $<
+
+all: ${PAGESET} index.html
+
+index.html: Home.html
+	ln -s Home.html index.html
+
+.PHONY: Download.ht
+
+Download.ht:
+	./Download/current.tcl
+
+clean:
+	rm -f ${PAGESET} index.html Download.ht
