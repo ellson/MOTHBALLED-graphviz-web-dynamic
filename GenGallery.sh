fd9454c6fc26ec8b844e6b26a6855812142c7a92
ellson <devnull@localhost>
2004-12-08 15:45:39 +0000

    add new partial transparency example

--- GenGallery.sh
+++ GenGallery.sh
@@ -3,25 +3,20 @@ let cnt=0;
 for i in Gallery/*/*small.png
 do
 	if (( cnt % 4 == 0)); then
-		echo
 		echo "<tbody><tr>"
 	fi
-	echo "<td align=center>"
 	title=`basename ${i%.small.png}`
 	full=${i%.small.png}.html
-	echo "<img src=\"$i\" alt=\"\" title=\"$title\">"
-	echo "</td>"
-	caption="$caption <td align=center> <A HREF=\"$full\">$title</A></td>"
+	echo "<td align=center><a href=\"$full\"><img src=\"$i\" border=\"0\" alt=\"\" title=\"$title\"></a></td>"
+	caption="$caption <td align=center> <a href=\"$full\">$title</a></td>"
 	if (( cnt % 4 == 3)); then
-		echo "</tr>"
-		echo "<tr> $caption </tr></tbody>"
+		echo "</tr><tr> $caption </tr></tbody>"
 		caption=""
 	fi
 	let cnt=cnt+1;
 done
 if (( ( cnt - 1 )  % 4 != 3)); then
-	echo "</tr>"
-	echo "<tr> $caption </tr></tbody>"
+	echo "</tr><tr> $caption </tr></tbody>"
 	caption=""
 fi
 echo "</table>"
