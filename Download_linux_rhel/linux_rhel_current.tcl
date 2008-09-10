8aa212144239942d49077a704419cded3e410a0c
ellson <devnull@localhost>
2008-09-10 13:01:45 +0000

    missing el4.noarch rpms

--- Download_linux_rhel/linux_rhel_current.tcl
+++ Download_linux_rhel/linux_rhel_current.tcl
@@ -24,7 +24,7 @@ set platform_directory_type_comments {
     FC7.x86_64 redhat/fc7/x86_64/os {fc7.x86_64.rpm fc7.noarch.rpm} "Fedora 7"
     EL5.i386 redhat/el5/i386/os {el5.i386.rpm el5.noarch.rpm} "Enterprise Linux 5 or later - uses fontconfig"
     EL5.x86_64 redhat/el5/x86_64/os {el5.x86_64.rpm el5.noarch.rpm} "Enterprise Linux 5 or later - uses fontconfig"
-    EL4.i386 redhat/el4/i386/os {el4.i386.rpm el5.noarch.rpm} "Enterprise Linux 4 or later - uses fontconfig"
+    EL4.i386 redhat/el4/i386/os {el4.i386.rpm el4.noarch.rpm} "Enterprise Linux 4 or later - uses fontconfig"
     EL4.x86_64 redhat/el4/x86_64/os {el4.x86_64.rpm el4.noarch.rpm} "Enterprise Linux 4 or later - uses fontconfig"
     EL3.i386 redhat/el3/i386/os {el3.i386.rpm el3.noarch.rpm} "Enterprise Linux 3 or later - uses fontconfig"
     EL3.x86_64 redhat/el3/x86_64/os {el3.x86_64.rpm el3.noarch.rpm} "Enterprise Linux 3 or later - uses fontconfig"
