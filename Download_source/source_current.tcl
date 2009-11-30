ed3235bd57a71df058a645488ae68a66388f6c81
erg <devnull@localhost>
2009-11-30 20:27:05 +0000

    Add smyrna dependencies

--- Download_source/source_current.tcl
+++ Download_source/source_current.tcl
@@ -147,8 +147,16 @@ puts $fout {
 	<dd><a href="http://www.pango.org/">http://www.pango.org/</a>
 	<dt>zlib-1.2.3.tar.gz [optional (required for libpng), recommended]
 	<dd><a href="http://www.zlib.net/">http://www.zlib.net/</a>
-	<dt>GTS [optional (required for sfdp, prism), recommended]
+	<dt>GTS [optional (required for sfdp, prism, smyrna), recommended]
 	<dd><a href="http://gts.sourceforge.net/">http://gts.sourceforge.net/</a>
+	<dt>GTK+ [optional (required for smyrna)]
+	<dd><a href="http://www.gtk.org/">http://www.gtk.org/</a>
+	<dt>GtkGLExt [optional (required for smyrna)]
+	<dd><a href="http://gtkglext.sourceforge.net/">http://gtkglext.sourceforge.net/</a>
+	<dt>Glade [optional (required for smyrna)]
+	<dd><a href="http://glade.gnome.org/">http://glade.gnome.org/</a>
+	<dt>Glut [optional (required for smyrna)]
+	<dd><a href="http://opengl.org/resources/libraries/glut/">http://opengl.org/resources/libraries/glut/</a>
 </dl>
 <h3>Tools</h3>
 <dl>
