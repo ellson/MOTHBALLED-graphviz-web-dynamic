349bcc53bf45ae578f4fb19ace79a3b30711f6c5
gviz <gviz@graphviz>
2011-07-29 17:13:22 -0400

    Added website changes

--- hg_update_html.sh
+++ hg_update_html.sh
@@ -1,11 +1,11 @@
 OUT=output_html
 cd /home/graphviz/myhttpd
 #cd /home/graphviz
-ssh gviz@hg "(/home/gviz/bin/hg_get_static_tgz.sh)" > $OUT 2>&1
+ssh gviz@hg "(/home/gviz/bin/hg_get_static_tgz.sh)" >> $OUT 2>&1
 scp -r gviz@hg:html.tgz . >> $OUT 2>&1
 tar xpzf html.tgz --atime-preserve 2>> $OUT
-rm html.tgz
+#rm html.tgz
 scp -r gviz@hg:doc.tgz . >> $OUT 2>&1
 tar xpzf doc.tgz --atime-preserve 2>> $OUT
-rm doc.tgz
+#rm doc.tgz
 
