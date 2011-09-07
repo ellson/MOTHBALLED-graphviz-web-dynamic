961195ef2316977a418d94a51683b6e68c306586
gviz <gviz@www.graphviz.org>
2011-09-07 18:10:26 -0400

    Added website changes

--- create_select_html_sp.sql
+++ create_select_html_sp.sql
@@ -3,8 +3,8 @@ DROP PROCEDURE select_html//
 CREATE PROCEDURE select_html(page_name text(50))
 BEGIN
 DECLARE x SMALLINT;
-select max(vid) into x from node_revisions where title=page_name;
-select  body from node_revisions where title=page_name and vid=x;
+select max(timestamp) into x from node_revisions where title=page_name;
+select  body from node_revisions where title=page_name and timestamp=x;
 END
 //
 
