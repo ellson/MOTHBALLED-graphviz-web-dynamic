58826d626e20382d0e79adc690894de41c7e8de5
gviz <gviz@www.graphviz.org>
2011-09-08 15:10:21 -0400

    Added website changes

--- create_update_html_sp.sql
+++ create_update_html_sp.sql
@@ -3,8 +3,8 @@ DROP PROCEDURE update_html//
 CREATE PROCEDURE update_html(page text(400000),page_name text(50))
 BEGIN
 DECLARE x SMALLINT;
-select max(timestamp) into x from node_revisions where title=page_name;
-Update node_revisions set body = page,teaser='' where title=page_name and timestamp=x;
+select max(vid) into x from node_revisions where title=page_name;
+Update node_revisions set body = page,teaser='' where title=page_name and vid=x;
 END
 //
 
