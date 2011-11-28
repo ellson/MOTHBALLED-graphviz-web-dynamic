927c551ff0fda91d2c6e0167a6d89ab96f77619a
gviz <gviz@www.graphviz.org>
2011-11-28 11:19:23 -0500

    Added website changes

--- Download_linux_ubuntu/linux_ubuntu_current.tcl
+++ Download_linux_ubuntu/linux_ubuntu_current.tcl
@@ -76,3 +76,81 @@ foreach {package platforms} $packages_platforms {
 puts $fout "</table>"
 	
 close $fout
+<!-- cut2 -->
+}
+
+puts $fout "<table frame=\"void\" rules=\"groups\" border=\"1\" width=\"100%\">"
+for {set i 0} {$i <= [llength $releases] / 2} {incr i} {
+    puts $fout "<colgroup><col></colgroup>"
+}
+
+foreach {package platforms} $packages_platforms {
+    puts $fout "<tbody>"
+    puts $fout "<tr>"
+    puts $fout "<th align=\"left\"><font size=\"+1\">$package</font></th>"
+    foreach {releasename releasedir} $releases {
+    	puts $fout "<th><font size=\"-1\">$releasename</font></th>"
+    }
+    puts $fout "</tr>"
+    puts $fout "</tbody>"
+    foreach {platform directory types comment} $platform_directory_type_comments {
+	if {[lsearch $platforms $platform] == -1} continue
+        puts $fout "<tbody>"
+        puts $fout "<tr><th align=\"right\"><font size=\"-1\">$platform</font></th>"
+        foreach {releasename releasedir} $releases {
+            puts $fout "<td align=\"left\" nowrap><font size=\"-1\">"
+            foreach type $types {
+                puts_latest $fout $docroot $releasedir/$directory $package $package_exclude $type
+            }
+            puts $fout "</font></td>"
+        }
+        puts $fout "</tr>"
+        puts $fout "</tbody>"
+    }
+    puts $fout "<tbody>"
+    puts $fout "<tr><td colspan=\"[expr {1 + ([llength $releases] / 2)}]\">&nbsp;</td></tr>"
+    puts $fout "</tbody>"
+}
+
+puts $fout "</table>"
+	
+close $fout
+<!-- cut2 -->
+}
+
+puts $fout "<table frame=\"void\" rules=\"groups\" border=\"1\" width=\"100%\">"
+for {set i 0} {$i <= [llength $releases] / 2} {incr i} {
+    puts $fout "<colgroup><col></colgroup>"
+}
+
+foreach {package platforms} $packages_platforms {
+    puts $fout "<tbody>"
+    puts $fout "<tr>"
+    puts $fout "<th align=\"left\"><font size=\"+1\">$package</font></th>"
+    foreach {releasename releasedir} $releases {
+    	puts $fout "<th><font size=\"-1\">$releasename</font></th>"
+    }
+    puts $fout "</tr>"
+    puts $fout "</tbody>"
+    foreach {platform directory types comment} $platform_directory_type_comments {
+	if {[lsearch $platforms $platform] == -1} continue
+        puts $fout "<tbody>"
+        puts $fout "<tr><th align=\"right\"><font size=\"-1\">$platform</font></th>"
+        foreach {releasename releasedir} $releases {
+            puts $fout "<td align=\"left\" nowrap><font size=\"-1\">"
+            foreach type $types {
+                puts_latest $fout $docroot $releasedir/$directory $package $package_exclude $type
+            }
+            puts $fout "</font></td>"
+        }
+        puts $fout "</tr>"
+        puts $fout "</tbody>"
+    }
+    puts $fout "<tbody>"
+    puts $fout "<tr><td colspan=\"[expr {1 + ([llength $releases] / 2)}]\">&nbsp;</td></tr>"
+    puts $fout "</tbody>"
+}
+
+puts $fout "</table>"
+	
+close $fout
