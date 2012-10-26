808e56a0bf1ed8323f5ff80e018f6b9fdcaf47ad
John Ellson <ellson@research.att.com>
2012-10-26 13:29:08 -0400

    removed commented lines

--- Download_windows/windows_current.tcl
+++ Download_windows/windows_current.tcl
@@ -29,8 +29,6 @@ proc checkdate {fnv} {
     foreach {fn v} $fnv {break}
     set lst [split $v {-.}]
     if {[llength $lst] >= 5} {
-#        foreach {. . dt tm td .} $lst {break}
-#        set time_stamp [clock scan "00:00:00 [string range $dt 0 3]-${tm}-${td}" -gmt 1]
         foreach {. . dt tm .} $lst {break}
         set time_stamp [clock scan [string range $dt 2 end]T${tm}00 -gmt 1]
         if {$time_stamp < $time_cutoff} {
