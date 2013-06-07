161089735e45573313ce2fe9c8b8147115946566
Dwight Perry <dperry@research.att.com>
2013-06-07 14:12:40 -0400

    Adding support for git

---
+++ git_copy_graphviz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+base=graphviz
+
+SERVER="hg.research.att.com"
+USR="gviz"
+OUT=~/output_$base
+> $OUT
+
+ssh $USR@$SERVER "(bin/git_get_repo_tgz.sh ${base})" > $OUT 2>&1
+scp -r $USR@$SERVER:${base}.tgz . >> $OUT 2>&1
+tar xzf "${base}.tgz" 2>> $OUT
+chmod 775 ${base} >> $OUT 2>&1
+touch ${base}
+rm ${base}.tgz >> $OUT 2>&1
+if [ ! -s $OUT ]
+then
+	rm $OUT
+fi
+
