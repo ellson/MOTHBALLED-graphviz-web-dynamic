f0ee90ff85247eeadd63b509af0e4adf80bb581b
gviz <gviz@graphviz.research.att.com>
2013-06-28 19:11:49 -0400

    Added website changes

--- git_update_html.sh
+++ git_update_html.sh
@@ -5,8 +5,8 @@ cd /home/graphviz/myhttpd
 ssh gviz@hg "(/home/gviz/bin/git_get_static_tgz.sh)" >> $OUT 2>&1
 scp -r gviz@hg:html.tgz . >> $OUT 2>&1
 tar xpzf html.tgz --atime-preserve 2>> $OUT
-#rm html.tgz
+rm html.tgz
 scp -r gviz@hg:doc.tgz . >> $OUT 2>&1
 tar xpzf doc.tgz --atime-preserve 2>> $OUT
-#rm doc.tgz
+rm doc.tgz
 
