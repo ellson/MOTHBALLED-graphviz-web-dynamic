4690a0d309eccff587d74c8016f69515077f7cea
John Ellson <ellson@research.att.com>
2013-10-03 12:18:58 -0400

    don't make changes on the webserver

--- git_update_dynamic.sh
+++ git_update_dynamic.sh
@@ -1,11 +1,11 @@
 OUTPUT=output_dynamic_html
 cd /home/graphviz/graphviz-web
 > $OUTPUT
-git commit -am "Added website changes"  >> $OUTPUT 2>&1
+# git commit -am "Added website changes"  >> $OUTPUT 2>&1
 # Get changes made by other users
 git pull  >> $OUTPUT 2>&1
 #Commit merged results - needed in hg but not in git
 #git commit -am "Merged website changes" >> $OUTPUT 2>&1
 #Push changes to server
-git push origin master>> $OUTPUT 2>&1
+# git push origin master>> $OUTPUT 2>&1
 
