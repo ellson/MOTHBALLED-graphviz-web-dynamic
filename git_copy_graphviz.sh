8d9cee4cbb8f891075c79d89cf06a0c01e4d9081
Emden R. Gansner <erg@alum.mit.edu>
2013-11-06 15:49:32 -0500

    Fix web building scripts to access git repository directly; copy of new version of doc directory

--- git_copy_graphviz.sh
+++ git_copy_graphviz.sh
@@ -8,12 +8,15 @@ USR="gviz"
 OUT=~/output_$base
 > $OUT
 
-ssh $USR@$SERVER "(bin/git_get_repo_tgz.sh ${base})" > $OUT 2>&1
-scp -r $USR@$SERVER:${base}.tgz . >> $OUT 2>&1
-tar xzf "${base}.tgz" 2>> $OUT
+#ssh $USR@$SERVER "(bin/git_get_repo_tgz.sh ${base})" > $OUT 2>&1
+#scp -r $USR@$SERVER:${base}.tgz . >> $OUT 2>&1
+#tar xzf "${base}.tgz" 2>> $OUT
+cd ${base}
+git pull >> $OUT 2>&1
+cd ..
 chmod 775 ${base} >> $OUT 2>&1
 touch ${base}
-rm ${base}.tgz >> $OUT 2>&1
+#rm ${base}.tgz >> $OUT 2>&1
 if [ ! -s $OUT ]
 then
 	rm $OUT
