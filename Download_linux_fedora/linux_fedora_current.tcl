768e281066c38a6557c1b436a42b6780526ffe84
John Ellson <ellson@research.att.com>
2017-07-19 11:10:53 -0400

    fix rpm name,version regexp to deal with python3

--- Download_linux_fedora/linux_fedora_current.tcl
+++ Download_linux_fedora/linux_fedora_current.tcl
@@ -95,7 +95,7 @@ proc checkdate {fnv} {
 #}
                                                                                 
 proc puts_latest {fout docroot dir package package_exclude type} {
-    set regexp {([-a-zA-Z]*)([-0-9.]*)([a-z][.a-z0-9_]*)}
+    set regexp {([-a-zA-Z0-9]*)(-[-0-9.]*)([a-z][.a-z0-9_]*)}
     if {![file exists $docroot/$dir]} {
         puts $fout "<font color=\"red\">Directory \"$docroot/$dir/\" was not found.</font>"
         return
